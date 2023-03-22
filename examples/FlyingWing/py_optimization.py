21e2bc80d5e834400001a049fcc2479c3556165c
jonrosen6 <32169929+jonrosen6@users.noreply.github.com>
2023-03-22 08:32:59 -0500

    Update py_optimization.py
    Plots the four main figures:
    - CL vs. h/b for constant angle of attack
    - After optimization (changing angle of attack) CL error vs. h/b while aiming to match the no ground effect CL (goal is zero)
    - Related to previous, "optimal" AoA vs. h/b
    - Related to previous, resulting CD_i vs. h/b

--- examples/FlyingWing/py_optimization.py
+++ examples/FlyingWing/py_optimization.py
@@ -12,12 +12,16 @@ import json
 import machupX as MX
 import numpy as np
 from scipy.optimize import minimize
-import os
+# import os
 import matplotlib.pyplot as plt
 
+# import sys
+# sys.path.append('c:/users/parina/documents/machupx')
 
-#Objective function to calculate lift force when given twist and height above ground. 
-def obj_fun(twist,dz, output='CL'): # add extra input to decide what value is output
+#%%
+
+# Objective function to calculate an output when given twist and HAG. 
+def obj_fun(twist,dz, output='CL'): #add extra input to decide what value is output
     
     with open('flying_wing.json', 'r') as openfile:        
         flying_wing_mod = json.load(openfile)
@@ -52,40 +56,47 @@ def obj_fun(twist,dz, output='CL'): # add extra input to decide what value is ou
     
     cd = FM_results["flying_wing"]["inviscid"]["CD"]["main_wing_left"] + FM_results["flying_wing"]["inviscid"]["CD"]["main_wing_right"]
     
-    if output=="CL":
-        return cl # add outputs for cd and cdi and work those into later code
-    elif output=="CD":
+    if output=="CL": #Coefficient of lift
+        return cl 
+    elif output=="CD": #Coefficient of drag
         return cd 
-    elif output=="CD_i":
+    elif output=="CD_i": #Induced drag
         return cd_i
     else:
         print('error')
     
-    
-CL = obj_fun(2, -100)
-print("CL:", CL)
+CL_0 = obj_fun(2, -100)
+print("CL:", CL_0)
 
-# #Plot for lift force as a function of h/b
-# dz = np.linspace(0.1, 16, 30)
-# results = []
-# for value in dz:
-#     lift_force = obj_fun(2,-value)
-#     results.append(lift_force)
-# plt.plot(dz/8, results)
-# plt.xlabel('h/b')
-# plt.ylabel('CL')
+#%%   Main figure 1 (constant angle of attack, how does CL change as we get close to the ground?)
+
+#Plot for lift force as a function of h/b
+dz = np.linspace(0.1, 16, 30)
+results = []
+for value in dz:
+    lift_force = obj_fun(2,-value)
+    results.append(lift_force)
+
+plt.figure()
+plt.plot(dz/8, results)
+plt.xlabel('h/b') # b=8 (wing span)
+plt.ylabel('CL')
 # plt.title("Lift Force as a Function of h/b")
 
-# # Plot for lift force as function of twist angle
-# fl = np.linspace(2,100,10)
-# fl_res = []
-# for i in fl:
-#     FL_2 = obj_fun(i, -100)
-#     fl_res.append(FL_2)
-# plt.plot(fl, fl_res)
-# plt.xlabel('Twist Angle')
-# plt.ylabel('Lift Force')
-# plt.title("Lift Force as a Function of Twist Angle")  
+#%%
+
+# Plot for lift force as function of twist angle
+alpha = np.linspace(-10,10,20)
+fl_res = []
+for i in alpha:
+    FL_2 = obj_fun(i, -100)
+    fl_res.append(FL_2)
+
+plt.figure()
+plt.plot(alpha, fl_res)
+plt.xlabel('Twist Angle')
+plt.ylabel('CL')
+plt.title("(no ground effect)")  
 
 #Notes:
 #In flying_wing.json 
@@ -94,29 +105,73 @@ print("CL:", CL)
 
 #%% Optimization 
 
-# CL_0 = 0.08443422032568976
-# xstar = []
-# fstar = []
+# CL_0 = 0.08443422032568976 # calculated above
+xstar = []
+fstar = []
 
-# for dz in np.linspace(0.8, 16, 40):
-#     dz = -dz
-#     fun = lambda tw: (obj_fun(tw, dz) - FL_0) ** 2
+dz = np.linspace(0.8, 16, 40)
 
-#     sol = minimize(fun, 2.0, args=(), method='trust-constr', jac=None, hess=None, hessp=None,
-#                     bounds=[(-10,10)], constraints=(), tol=1e-18, callback=None, options=None)
+for ii in dz:
+    fun = lambda tw: (obj_fun(tw, -ii) - CL_0) ** 2
+
+    sol = minimize(fun, 2.0, args=(), method='trust-constr', jac=None, hess=None, hessp=None,
+                    bounds=[(-10,10)], constraints=(), tol=1e-18, callback=None, options=None)
     
-#     x, f = sol.x, sol.fun
-#     xstar.append(x)
-#     fstar.append(f)
+    x, f = sol.x, sol.fun
+    xstar.append(x)
+    fstar.append(f)
 
-# dz = np.linspace(0.8, 16, 40)
-# plt.plot(dz, fstar)
-# plt.xlabel("Height Above Ground")
-# plt.ylabel("Lift Force")
+#%%    Main figure 2 (if we allow angle of attack to change, can we match the ground effect CL with CL high above ground at angle of attack=2deg)
+
+#Plot for Lift Force as a function of HAB with angle of attack held constant
+# dz = np.linspace(0.8, 16, 40) # no need to redefine this
+
+plt.figure()
+plt.plot(dz/8, fstar)
+plt.xlabel("h/b") # b=8 (wing span)
+plt.ylabel(r"$(\Delta CL)^2$") # actually plotting the squared error here (want zero)
 # plt.title("Lift force as a Function of Height Above Ground")
-# plt.show()
+plt.show()
 
-# Graph with heatmap
+
+#%%    Main figure 3 (corresponding to the previous figure, what are the AoA values?)
+
+plt.figure()
+plt.plot(dz/8, xstar)
+plt.xlabel("h/b") # b=8 (wing span)
+plt.ylabel(r"$\alpha$")
+
+
+#%%    Main figure 4 (corresponding to the previous two figures, what is the induced drag?)
+
+CD_i = []
+
+for ii in range(len(xstar)):
+    CD_i.append(obj_fun(xstar[ii], -dz[ii], output='CD_i'))
+
+
+plt.figure()
+plt.plot(dz/8, CD_i)
+plt.xlabel("h/b") # b=8 (wing span)
+plt.ylabel(r"$CD_i$")
+
+
+
+
+
+
+
+
+
+
+
+
+#%%   I think everything below here is old
+
+
+
+# #Graph with heatmap
+
 # FL_0 = 16.055
 # dz_vals = np.linspace(0.8, 16, 40)
 # tw_vals = np.linspace(-10, 10, 21)
@@ -137,6 +192,7 @@ print("CL:", CL)
 # ax.set_title('Optimized Lift Force')
 # plt.show()
 
+# #%%
 # Graph with lift force and HAB
 
 # FL_0 = 16.055
@@ -162,6 +218,7 @@ print("CL:", CL)
 # plt.title("Optimized lift Force as a Function of Height Above Ground")
 # plt.show()
 
+# #%%
 
 # FL_0 = 16.055
 # dz_vals = np.linspace(0.8, 16, 10)
@@ -183,5 +240,3 @@ print("CL:", CL)
 # ax.set_ylabel('Angle of Attack (deg)')
 # ax.set_title('Optimized Angle of Attack for Minimum Lift Force')
 # plt.show()
-
-
